fecb45c111d9e0a635a5d9e7a649edef6f1fa5c9
Arthur L <31710356+arthur-lemaire@users.noreply.github.com>
2024-01-25 22:02:38 +0100

    Ajoutez des fichiers projet.

---
+++ WebApplication2/Contact.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Recaptcha.Web;
+
+namespace WebApplication2
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnValider_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(Recaptcha1.Response))
+            {
+
+            }
+            else
+            {
+                var result = Recaptcha1.Verify();
+                if (result.Success)
+                {
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+    }
+}
---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using Recaptcha.Web.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
